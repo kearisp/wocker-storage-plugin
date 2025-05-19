19c5c04c165864ca53a593bcf83edb3415f31956
Kris Papercut <krispcut@gmail.com>
2025-05-19 08:53:35 +0000

    v1.0.8

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import {Plugin, PluginConfigService} from "@wocker/core";
-
 import {StorageController} from "./controller/StorageController";
 import {StorageService} from "./services/StorageService";
 
