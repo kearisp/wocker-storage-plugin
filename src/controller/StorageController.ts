f545ef66aa540b4bfbfe18be133d1065e8a60696
Kris Papercut <krispcut@gmail.com>
2026-06-14 15:05:36 +0300

    v1.0.10 (#11)

--- src/controller/StorageController.ts
+++ src/controller/StorageController.ts
@@ -6,7 +6,8 @@ import {
     Option
 } from "@wocker/core";
 import {StorageService} from "../services/StorageService";
-import {StorageType} from "../makes/Storage";
+import {StorageType} from "../types/StorageType";
+import {StorageStyle} from "../types/StorageStyle";
 
 
 @Controller()
@@ -21,43 +22,29 @@ export class StorageController {
     public async create(
         @Param("name")
         name?: string,
-        @Option("type", {
-            type: "string",
-            description: "Storage type"
-        })
+        @Option("type")
+        @Description(`Storage type (${StorageType.values().join(", ")})`)
         type?: StorageType,
-        @Option("user", {
-            type: "string",
-            alias: "u",
-            description: "User name"
-        })
+        @Option("style")
+        @Description(`Storage style (${StorageStyle.values().join(", ")})`)
+        style?: StorageStyle,
+        @Option("user", "u")
+        @Description("User name")
         username?: string,
-        @Option("password", {
-            type: "string",
-            alias: "p",
-            description: "Password"
-        })
+        @Option("password", "p")
+        @Description("Password")
         password?: string,
-        @Option("image", {
-            type: "string",
-            alias: "i",
-            description: "The image name to start the service with"
-        })
-        imageName?: string,
-        @Option("image-version", {
-            type: "string",
-            alias: "I",
-            description: "The image version to start the service with"
-        })
-        imageVersion?: string
+        @Option("image", "i")
+        @Description("The image name to start the service with")
+        image?: string
     ): Promise<void> {
         await this.storageService.create({
             name,
             type,
+            style,
             username,
             password,
-            imageName,
-            imageVersion
+            image
         });
     }
 
@@ -66,17 +53,11 @@ export class StorageController {
     public async destroy(
         @Param("name")
         name: string,
-        @Option("yes", {
-            type: "boolean",
-            alias: "y",
-            description: "Confirm destruction without prompting for confirmation."
-        })
+        @Option("yes", "y")
+        @Description("Confirm destruction without prompting for confirmation.")
         yes?: boolean,
-        @Option("force", {
-            type: "boolean",
-            alias: "f",
-            description: "Force deletion even if the storage is set as default"
-        })
+        @Option("force", "f")
+        @Description("Force deletion even if the storage is set as default")
         force?: boolean
     ): Promise<void> {
         await this.storageService.destroy(name, yes, force);
@@ -87,30 +68,21 @@ export class StorageController {
     public async upgrade(
         @Param("name")
         name?: string,
-        @Option("volume", {
-            type: "string",
-            alias: "v",
-            description: "The volume to start the service with"
-        })
+        @Option("style")
+        @Description(`Storage style (${StorageStyle.values().join(", ")})`)
+        style?: StorageStyle,
+        @Option("volume", "v")
+        @Description("The volume to start the service with")
         volume?: string,
-        @Option("image", {
-            type: "string",
-            alias: "i",
-            description: "The image name to start the service with"
-        })
-        imageName?: string,
-        @Option("image-version", {
-            type: "string",
-            alias: "I",
-            description: "The image version to start the service with"
-        })
-        imageVersion?: string
+        @Option("image", "i")
+        @Description("The image name to start the service with")
+        image?: string
     ): Promise<void> {
         this.storageService.upgrade({
             name,
+            style,
             volume,
-            imageName,
-            imageVersion
+            image
         });
     }
 
@@ -125,11 +97,8 @@ export class StorageController {
     public async start(
         @Param("name")
         name?: string,
-        @Option("restart", {
-            type: "boolean",
-            alias: "r",
-            description: "Restart the storage service if it is already running"
-        })
+        @Option("restart", "r")
+        @Description("Restart the storage service if it is already running")
         restart?: boolean
     ): Promise<void> {
         await this.storageService.start(name, restart);
@@ -142,6 +111,32 @@ export class StorageController {
         name?: string
     ): Promise<void> {
         await this.storageService.stop(name);
+    }
+
+    @Command("storage:create-bucket [bucket]")
+    @Command("storage:create-bucket <name>/<bucket>")
+    public async createBucket(
+        @Param("name")
+        name?: string,
+        @Param("bucket")
+        bucket?: string
+    ) {
+        await this.storageService.createBucket(name, bucket);
+    }
+
+    @Command("storage:delete-bucket [bucket]")
+    @Command("storage:delete-bucket <name>/<bucket>")
+    public async deleteBucket(
+        @Param("name")
+        name?: string,
+        @Param("bucket")
+        bucket?: string,
+        @Option("yes", "y")
+        yes?: boolean,
+        @Option("force", "f")
+        force?: boolean
+    ) {
+        await this.storageService.deleteBucket(name, bucket, yes, force);
     }
 
     @Command("storage:use <name>")
--- src/env.ts
+++
@@ -1,2 +0,0 @@
-export const STORAGE_TYPE_MINIO = "minio";
-export const STORAGE_TYPE_REDIS = "redis";
